6502124a5323ac051a1de04a83c38e27e59c7fd8
angelfish2510 <angelisa_herring@yahoo.com>
2022-11-23 14:05:34 -0800

    addiitional animations for main page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,77 @@
-const myModal = document.getElementById('FEWD')
-const myInput = document.getElementById('myInput')
+// welcome notification
+
+const topbackground = document.getElementById("frheading");
+
+function welcomeNotification() {
+    const notification = new Notification("Welcome to the world of Flying Rock!", {
+        body: "Thank you for your interest in my work.  I am very much looking forward to contributing to your next project.",
+        icon: "/images/ruru.jpg"
+    });
+}
+
+topbackground.addEventListner('click', welcomeNotification);
+
+
+
+
+
+
+
+
+// const myModal = document.getElementById('FEWD')
+// const myInput = document.getElementById('myInput')
 
 // myModal.addEventListener('shown.bs.modal', () => {
 //   myModal.focus()
 // })
 
 // $modal-fade-transform: scale(.8)
+
+
+// display modal function with parameter index
+// function displayModal(i) {
+//     index = i;
+//     let { name, dob, phone, email, location: {city, street, state, postcode}, picture } = employees[index];
+
+//     let date = new Date(dob.date);
+
+//     const modalHTML = `
+//         <img class="avatar" src="${picture.large}" alt="employee photo">
+//         <div class="text-container">
+//             <h2 class="name">${name.first} ${name.last}</h2>
+//             <p class="email">${email}</p>
+//             <p class="address">${city}</p>
+//             <hr />
+//             <p class="phone">${phone}</p>
+//             <p class="address">${street.number} ${street.name}, ${state}, ${postcode}</p>
+//             <p class="birthday">Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+//         </div>
+//     `;
+
+//     overlay.classList.remove("hidden");
+//     modalContainer.innerHTML = modalHTML;
+// }
   
+
+// smallworld animation
+
+// let getSiblings = function (elem) {
+
+// 	// Setup siblings array and get the first sibling
+// 	let siblings = [];
+// 	let sibling = elem.parentNode.firstChild;
+
+// 	// Loop through each sibling and push to the array
+// 	while (sibling) {
+// 		if (sibling.nodeType === 1 && sibling !== elem) {
+// 			siblings.push(sibling);
+// 		}
+// 		sibling = sibling.nextSibling
+// 	}
+
+// 	return siblings;
+
+// };
+
+// let elem = document.querySelector('#topHuman');
+// let siblings = getSiblings(elem);
